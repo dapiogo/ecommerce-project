49c8ba896b8ff12095694022443a99e886b4489f
dawidgola <92149687+dawidgola@users.noreply.github.com>
2023-01-08 14:29:03 +0100

    feat: add simple form

---
+++ components/CheckoutForm/index.tsx
@@ -0,0 +1,94 @@
+import { FormEventHandler } from 'react';
+
+// interface CheckoutFormData {
+//   firstName: string;
+//   lastName: string;
+//   emailAddress: string;
+//   address: string;
+// }
+
+export const CheckoutForm = () => {
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    console.log(event);
+  };
+  return (
+    <form onSubmit={handleSubmit} className="w-full max-w-lg">
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="grid-first-name">
+            First Name
+          </label>
+          <input
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            id="grid-first-name"
+            type="text"
+            placeholder="Jane"
+            name="firstName"
+          />
+          <p className="text-gray-600 text-xs italic">
+            Please enter your first name.
+          </p>
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="grid-last-name">
+            Last Name
+          </label>
+          <input
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-last-name"
+            type="text"
+            placeholder="Doe"
+            name="lastName"
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="grid-email">
+            Email
+          </label>
+          <input
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-email"
+            type="email"
+            placeholder="jane.doe@example.com"
+            name="emailAddress"
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <label
+            className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            htmlFor="grid-address">
+            Address
+          </label>
+          <textarea
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-address"
+            placeholder="1234 Main St."
+            name="address"
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap -mx-3  mt-6">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit">
+          Send
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default CheckoutForm;
